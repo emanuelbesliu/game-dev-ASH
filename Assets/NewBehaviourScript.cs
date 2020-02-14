570dd08aca9a4aa4d86a7d0d4d9a188378796cf3
emanuelbesliu <emanuelbesliuph@gmail.com>
2020-02-14 21:10:23 +0000

    New Background + jump

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    public float speed = 1.5f;
+    public Vector2 jumpHeight;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +15,27 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        print("Cubify");
-	transform.Translate(1f*Time.deltaTime, 0f, 0f);
-    }
+	 if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))  //makes player jump
+    	 {
+        	GetComponent<Rigidbody2D>().AddForce(jumpHeight, ForceMode2D.Impulse);
+	 }
+         print("Cubify");
+	 //transform.Translate(1f*Time.deltaTime, 0f, 0f);
+    	 if (Input.GetKey(KeyCode.LeftArrow))
+         {
+             transform.position += Vector3.left * speed * Time.deltaTime;
+         }
+         if (Input.GetKey(KeyCode.RightArrow))
+         {
+             transform.position += Vector3.right * speed * Time.deltaTime;
+         }
+         if (Input.GetKey(KeyCode.UpArrow))
+         {
+             transform.position += Vector3.up * speed * Time.deltaTime;
+         }
+         if (Input.GetKey(KeyCode.DownArrow))
+         {
+             transform.position += Vector3.down * speed * Time.deltaTime;
+         }	
+   }
 }
