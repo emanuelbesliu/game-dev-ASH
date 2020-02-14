b62306ccf2cbad4968006e96782f56f43b145e97
emanuelbesliu <emanuelbesliuph@gmail.com>
2020-02-14 10:50:21 +0000

    Some assets plus a cube and basic movement no controllers yet/first coding session

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -7,12 +7,13 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+       print("Cube here");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        print("Cubify");
+	transform.Translate(1f*Time.deltaTime, 0f, 0f);
     }
 }
