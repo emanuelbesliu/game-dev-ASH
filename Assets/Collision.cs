1e93406cb8f613aa716a9aa8a789f63ca0632247
Eduard Alexandru Pop <steel1san@gmail.com>
2020-02-16 20:59:21 +0000

    Added movement, jump, dash functionalities

---
+++ Assets/Collision.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    public LayerMask groundLayer;
+
+    public bool onGround;
+
+    public float collisionRadius = 0.25f;
+    public Vector2 bottomOffset;
+    private Color debugCollisionColor = Color.red;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        onGround = Physics2D.OverlapCircle((Vector2)transform.position + bottomOffset, collisionRadius, groundLayer);
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        var positions = new Vector2[] { bottomOffset };
+
+        Gizmos.DrawWireSphere((Vector2)transform.position + bottomOffset, collisionRadius);
+    }
+}
---
+++ Assets/Movement.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private Collision coll;
+    [HideInInspector]
+    public Rigidbody2D rb;
+
+    [Space]
+    [Header("Forces")]
+    public float speed = 10;
+    public float wallJumpLerp = 10;
+    public float jumpForce = 15;
+    public float dashSpeed = 20;
+
+
+    [Space]
+    [Header("Checks")]
+    public bool canMove;
+    public bool isDashing;
+
+    [Space]
+
+    private bool groundTouch;
+    private bool hasDashed;
+
+
+    void Start()
+    {
+        coll = GetComponent<Collision>();
+        rb = GetComponent<Rigidbody2D>();
+        canMove = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        float xRaw = Input.GetAxisRaw("Horizontal");
+        float yRaw = Input.GetAxisRaw("Vertical");
+
+        Vector2 dir = new Vector2(x, y);
+
+        Walk(dir);
+
+        rb.gravityScale = 3;
+
+        if (coll.onGround && !isDashing)
+        {
+            GetComponent<Jump>().enabled = true;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (coll.onGround)
+                Jump(Vector2.up);
+        }
+
+        if(Input.GetButtonDown("Boost")&&!hasDashed)
+        {
+            if (xRaw != 0 || yRaw != 0)
+                Dash(xRaw, yRaw);
+        }
+
+        
+
+        if(coll.onGround && !groundTouch)
+        {
+            GroundTouch();
+            groundTouch = true;
+        }
+
+        if(!coll.onGround && groundTouch)
+        {
+            groundTouch = false;
+        }
+
+
+    }
+    void GroundTouch()
+    {
+        hasDashed = false;
+        isDashing = false;
+    }
+
+    private void Walk(Vector2 dir)
+    {
+        if (!canMove)
+            return;
+        rb.velocity = Vector2.Lerp(rb.velocity, (new Vector2(dir.x * speed, rb.velocity.y)), wallJumpLerp * Time.deltaTime);
+    }
+
+    private void Jump(Vector2 dir)
+    {
+        rb.velocity = new Vector2(rb.velocity.x, 0);
+        rb.velocity += dir * jumpForce;
+    }
+
+    private void Dash(float x, float y)
+    {
+        hasDashed = true;
+
+        rb.velocity = Vector2.zero;
+        Vector2 dir = new Vector2(x, y);
+
+        rb.velocity += dir.normalized * dashSpeed;
+        StartCoroutine(DashWait());
+    }
+
+    IEnumerator DashWait()
+    {
+        StartCoroutine(GroundDash());
+
+        rb.gravityScale = 0;
+        GetComponent<Jump>().enabled = false;
+        isDashing = true;
+        yield return new WaitForSeconds(.3f);
+
+        rb.gravityScale = 3;
+        GetComponent<Jump>().enabled = true;
+        isDashing = false;
+    }
+
+    IEnumerator GroundDash()
+    {
+        yield return new WaitForSeconds(.15f);
+        if (coll.onGround)
+            hasDashed = false;
+    }
+
+    IEnumerator DisableMovement(float time)
+    {
+        canMove = false;
+        yield return new WaitForSeconds(time);
+        canMove = true;
+    }
+}
